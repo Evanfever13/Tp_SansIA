3360555c4f6e3201da226dc383ab4a51ec4d58fd
Hugo PAULIER <hugo.paulier@ynov.com>
2025-10-15 13:39:00 +0200

    Exercice5-HP

---
+++ Exercice5.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func PrintHollowDiamond(r int) {
+	nbr := r
+	if nbr%2 == 0 {
+		nbr++
+	}
+	espace := nbr / 2
+	for toto := 0; toto < nbr; toto++ {
+		for tata := 0; tata < nbr; tata++ {
+			if toto+tata == espace || tata-toto == espace || toto-tata == espace || toto+tata == nbr+espace-1 {
+				fmt.Print("*")
+			} else {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Println()
+	}
+}
